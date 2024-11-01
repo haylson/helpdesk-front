fd967a04720cbffd5d63cc1a1c49c975387c9b78
haylson <haylson@gmail.com>
2024-11-01 14:51:46 -0300

    Rota para componente chamado-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ import { ClienteDeleteComponent } from './components/cliente/cliente-delete/clie
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
-//import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 //import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-read.component';
 //import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
 
@@ -34,7 +34,7 @@ const routes: Routes = [
       { path: 'clientes/delete/:id', component: ClienteDeleteComponent },
 
       { path: 'chamados',                       component:     ChamadoListComponent },
-      //{ path: 'chamados/create',                component:   ChamadoCreateComponent },
+      { path: 'chamados/create',                component:   ChamadoCreateComponent },
       //{ path: 'chamados/update/:id',            component:   ChamadoUpdateComponent },
       //{ path: 'chamados/read/:id',              component:     ChamadoReadComponent }
     ]
